16b8c2a2b6d390a26e6b8e1b245ec3d335f41590
Atul V Chandran <atulvc2001@gmail.com>
2021-06-09 09:58:48 +0530

    reformatted main.py

--- URLshortner/main.py
+++ URLshortner/main.py
@@ -2,23 +2,32 @@ import contextlib
 import sys
 
 try:
+
     from urllib.parse import urlencode
+    
 except ImportError:
+
     from urllib import urlencode
+
 try:
+    
     from urllib.request import urlopen
+
 except ImportError:
+    
     from urllib2 import urlopen
 
+
 def make_tiny(url):
-    request_url = ('http://tinyurl.com/api-create.php?' + 
-	urlencode({'url':url}))
+    request_url = "http://tinyurl.com/api-create.php?" + urlencode({"url": url})
     with contextlib.closing(urlopen(request_url)) as response:
-        return response.read().decode('utf-8')
+        return response.read().decode("utf-8")
+
 
 def main():
     for tinyurl in map(make_tiny, sys.argv[1:]):
         print(tinyurl)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
